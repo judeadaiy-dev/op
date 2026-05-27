0a9d140fbb1d39dd646cb17faee14be78d82c15a
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-27 17:53:51 +0300

    Update search_screen.dart

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -4,7 +4,7 @@ import '../providers/auth_provider.dart';
 import '../widgets/glass_container.dart';
 import '../theme/app_theme.dart';
 import '../utils/supabase_client.dart';
-
+import 'package:chat_app/theme/app_colors.dart';
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
