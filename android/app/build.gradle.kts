5aa5a4744d349e118377884bc4ef179e03ad93dd
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-30 14:19:20 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,31 +1,33 @@
-apply plugin: 'com.android.application'
-apply plugin: 'kotlin-android'
-apply from: "$flutterRoot/packages/flutter_tools/gradle/flutter.gradle"
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
 
 android {
-    namespace "com.judeadaiy.chat_app"
-    compileSdk 34 // رفعنا الإصدار ليكون متوافقاً
+    namespace = "com.judeadaiy.chat_app"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.judeadaiy.chat_app"
-        minSdk 23
-        targetSdk 34
-        versionCode 1
-        versionName "1.0.0"
-        multiDexEnabled true // ضروري جداً للمشاريع الكبيرة
+        applicationId = "com.judeadaiy.chat_app"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+        multiDexEnabled = true
     }
 
     compileOptions {
-        coreLibraryDesugaringEnabled true
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = '17'
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.0.4'
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
